ea4346f8ffb2cf034fae6369fe9640aef712a0e9
Issabelle Rovin Santos <11906913@benilde.edu.ph>
2021-03-25 15:52:32 +0800

    Initial commit

---
+++ ProjectCare01/Controllers/AboutUs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectCare01.Controllers
+{
+	public class AboutUs : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
