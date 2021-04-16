4ebdd7663c82f000912b0c54d5d5dd3ebad1b676
Issabelle Rovin Santos <11906913@benilde.edu.ph>
2021-04-16 16:07:30 +0800

    Revert "updated for the missing files of concerns page"
    This reverts commit b30f9e0572deec6114d699d15708ed09e6a1424e.

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -18,6 +18,5 @@ namespace ProjectCare01.Data
         public DbSet<Order>Orders { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
-        public DbSet<Concern> Concerns { get; set; }
     }
 }
