776e874ccd464b303e74aed252dc473052d54b8e
Issabelle Rovin Santos <11906913@benilde.edu.ph>
2021-04-20 22:47:57 +0800

    removed the email notif in concern

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,8 +7,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ProjectCare01.Models;
 
-using System.Net;
-using System.Net.Mail;
 
 namespace ProjectCare01.Controllers
 {
@@ -36,37 +34,7 @@ namespace ProjectCare01.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        [HttpPost]
-        public IActionResult Concern(Concern record)
-        {
-            using (MailMessage mail = new MailMessage("benilde.web.development@gmail.com", record.Email))
-            {
-                mail.Subject = record.OrderNo;
 
-                string message = "Hello," + record.Name + "!<br/><br/>" +
-                    "We have received your inquiry. Here are the details: <br/><br/>" +
-                    "Contact Number: <strong>" + record.ContactNo + "</strong><br/>" +
-                    "Message : <br/><strong>" + record.Problem + "</strong><br/>" +
-                    "Please wait for our reply. Thank you!";
-                mail.Body = record.Problem;
-                mail.IsBodyHtml = true;
-
-                using(SmtpClient smtp = new SmtpClient())
-				{
-                    smtp.Host = "smtp.gmail.com";
-                    smtp.EnableSsl = true;
-                    NetworkCredential NetworkCred =
-                        new NetworkCredential("benilde.wed.development@gmail.com", "TeamProjectCare01");
-                    smtp.UseDefaultCredentials = true;
-                    smtp.Credentials = NetworkCred;
-                    smtp.Port = 587;
-                    smtp.Send(mail);
-                    ViewBag.Message = "Inquiry Sent.";
-				}
-
-            }
-
-            return View();
         }
     }
-}
+
